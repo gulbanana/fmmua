183a546eaf66fe915d2c134d61c0d445d30e3151
Thomas Castiglione <castiglione@mac.com>
2020-08-27 22:16:47 +0800

    preserve RollDialog defaults across instances

--- src/dice/RollDialog.ts
+++ src/dice/RollDialog.ts
@@ -2,19 +2,32 @@ import * as dice from './dice.js';
 import { RollTag } from './StrikeRoll.js';
 
 export interface RollDialogResult {
-    execute: boolean;
+    type: RollTag;
     advantage: boolean;
     disadvantage: boolean;
+    flavor: string | undefined;
 }
 
 export default class RollDialog extends Dialog {
+    static defaults: RollDialogResult = {
+        type: "skilled",
+        advantage: false,
+        disadvantage: false,
+        flavor: undefined
+    }
+
     constructor(dialogData = {}, options = {}) {
         super(dialogData, options);
         this.options.classes = ["fmmua", "dialog"];
     }
 
     static async run(flavor?: string): Promise<void> {
-        const content = await renderTemplate("systems/fmmua/dice/RollDialog.html", { flavor });
+        let data = this.defaults;
+        if (typeof flavor === "string" && flavor.length > 0) {
+            data.flavor = flavor;
+        }
+
+        const content = await renderTemplate("systems/fmmua/dice/RollDialog.html", data);
 
         return new Promise((resolve) => {
             const dialog = new this({
@@ -24,13 +37,15 @@ export default class RollDialog extends Dialog {
                     roll: {
                         icon: `<i class="fas fa-dice"></i>`,
                         label: "Roll",
-                        callback: (html: JQuery<HTMLElement>) => {
-                            dice.strikeRoll(
-                                html[0].querySelector<HTMLInputElement>(".fmmua-advantage")!.checked,
-                                html[0].querySelector<HTMLInputElement>(".fmmua-disadvantage")!.checked,
-                                html[0].querySelector<HTMLInputElement>(".fmmua-flavor")!.value,
-                                html[0].querySelector<HTMLSelectElement>(".fmmua-roll-type")!.value as RollTag
-                            );                            
+                        callback: async (html: JQuery<HTMLElement>) => {
+                            let result = {
+                                type: html[0].querySelector<HTMLSelectElement>(".fmmua-roll-type")!.value as RollTag,
+                                advantage: html[0].querySelector<HTMLInputElement>(".fmmua-advantage")!.checked,
+                                disadvantage: html[0].querySelector<HTMLInputElement>(".fmmua-disadvantage")!.checked,
+                                flavor: html[0].querySelector<HTMLInputElement>(".fmmua-flavor")!.value
+                            };
+                            this.defaults = result;
+                            await dice.strikeRoll(result.advantage, result.disadvantage, result.flavor, result.type);
                             resolve();
                         }
                     },
