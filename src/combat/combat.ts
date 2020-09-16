f7e3b8591a3fa6a8f1d6c5dc6217e47820184fa7
Thomas Castiglione <castiglione@mac.com>
2020-09-16 22:16:53 +0800

    d6 instead of d20 rolls

--- src/combat/combat.ts
+++ src/combat/combat.ts
@@ -2,6 +2,7 @@ import StrikeCombat from "./StrikeCombat.js";
 import StrikeTracker from "./StrikeTracker.js";
 
 export function init() {
+    CONFIG.Combat.initiative.decimals = 0;
     CONFIG.Combat.entityClass = StrikeCombat;
     CONFIG.ui.combat = StrikeTracker;
 }
